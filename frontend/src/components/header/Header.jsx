57e4ea5d4cc7f3b51ad52f15efa88df4e91bf9a1
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-01-20 14:01:18 -0300

    refactor: front page

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import ellipse from "../../images/ellipse-lg-03.png"
+import ellipse from "../../images/ellipse-lg-04.png"
 
 
 const Header = () => {
@@ -9,9 +9,9 @@ const Header = () => {
 				<img src={ellipse}/>
 			</div>
       <div className="inner-header-container">
-        <p>Energia Sustentável</p>
-        <p>Emissão de Carbono</p>
-        <p>Reciclagem</p>
+        <p>Energia Sustentável<span>.</span></p>
+        <p>Emissão de Carbono<span>.</span></p>
+        <p>Reciclagem<span>.</span></p>
       </div>
     </div>
   );
