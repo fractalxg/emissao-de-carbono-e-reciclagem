26b67f8332cbd422684760eec350c87eb8d9829c
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-01-20 13:14:49 -0300

    refactor: front page

--- frontend/src/components/body/Presentation.jsx
+++ frontend/src/components/body/Presentation.jsx
@@ -1,9 +1,22 @@
 import "./Presentation.css";
-import teste from "../../images/ellipse-lg.png"
+import es_01 from "../../images/es-01.png"
+import es_02 from "../../images/es-02.png"
+import ec_01 from "../../images/ec-01.png"
+import ec_02 from "../../images/ec-02.png"
+import rc_01 from "../../images/rc-01.png"
+import rc_02 from "../../images/rc-02.png"
 
 const Presentation = () => {
   return (
     <div className="presentation-container">
+			<div className="presentation-image-content">
+				<img src={es_01} style={{paddingBottom:"100px"}}/>
+				<img src={es_02} style={{paddingTop:"100px"}}/>
+				<img src={ec_01} style={{paddingBottom:"100px"}}/>
+				<img src={ec_02} style={{paddingTop:"100px"}}/>
+				<img src={rc_02} style={{paddingBottom:"100px"}}/>
+				<img src={rc_01} style={{paddingTop:"100px"}}/>
+			</div>
     </div>
   );
 };
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,13 @@
 import "./Header.css";
+import ellipse from "../../images/ellipse-lg-03.png"
+
 
 const Header = () => {
   return (
     <div className="header-container">
+			<div className="header-logo">
+				<img src={ellipse}/>
+			</div>
       <div className="inner-header-container">
         <p>Energia Sustentável</p>
         <p>Emissão de Carbono</p>
