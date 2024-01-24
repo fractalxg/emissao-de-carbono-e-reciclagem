c2a229c43d8260b719ed8003c65472d6c0391084
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-01-24 20:23:04 -0300

    feat and style: carbon emission

--- frontend/src/components/body/CarbonEmission.jsx
+++ frontend/src/components/body/CarbonEmission.jsx
@@ -1,15 +1,35 @@
-import "./CarbonEmission.css"
+import "./CarbonEmission.css";
+import ec_02_lg from "../../images/ec-02-lg.png";
 
 const CarbonEmission = () => {
   return (
     <div className="carbon-emission-container" id="carbonEmission">
-        {/* <div className="carbon-emission-calculator">
-					<input type="text"></input>
-					<input type="text"></input>
-					<button>calcular</button>
-				</div> */}
-    </div>
-  )
-}
+      <div className="carbon-emission-header">
+        <div className="carbon-emission-bar">
+          <div className="up-carbon-emission-bar"></div>
+          <div className="bottom-carbon-emission-bar">
+            <h2>Emissão de Carbono<span>.</span></h2>
+          </div>
+        </div>
+        <div className="carbon-emission-image">
+        <img src={ec_02_lg} />
 
-export default CarbonEmission
+        </div>
+      </div>
+      <div className="carbon-emission-calculator">
+        <div className="carbon-emission-origin-container">
+          <label>Origem:</label>
+
+          <input type="text" placeholder="Digite um endereço"></input>
+        </div>
+
+        <div className="carbon-emission-destiny-container">
+          <label>Destino:</label>
+          <input type="text" placeholder="Digite um endereço"></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarbonEmission;
